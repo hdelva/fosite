51f050faa3141be1dd012f64d569c9591938174c
harm.delva@ugent.be <thae4iey>
2016-11-09 22:22:53 +0100

    Started fleshing out the KnowledgeBase, added Scope struct

--- rust/fosite/src/main.rs
+++ rust/fosite/src/main.rs
@@ -1,27 +1,31 @@
 #![allow(dead_code)]
+#![feature(box_syntax)]
 
 use std::collections::HashSet;
 use std::collections::HashMap;
+use std::collections::hash_map::Entry::{Occupied, Vacant};
 
 /// type aliases
 
 type Type = i16;
+
+//todo change to enum; Data -> &Object / Code -> Callable
 type Pointer = i16;
 
 /// Placeholders
 
 trait KnowledgeBase {
-    fn link_objects(&mut self, parent: &Pointer, child: &Pointer);
+    fn link_objects(&mut self, parent: &Pointer, child: &Pointer); //
 
-    fn limit_types(&mut self, address: &Pointer, limits: &HashSet<Type>);
+    fn limit_types(&mut self, address: &Pointer, limits: &HashSet<Type>); //
 
-    fn function_definition(&self, address: &Pointer) -> &FunctionDefinition;
+    fn function_definition(&self, address: &Pointer) -> &FunctionDefinition; //
 
-    fn builtin_function(&self, address: &Pointer) -> &BuiltinFunction;
+    fn builtin_function(&self, address: &Pointer) -> &BuiltinFunction; //
 
-    fn dereference(&self, address: &Pointer) -> &Object;
+    fn dereference(&self, address: &Pointer) -> &Object; //
 
-    fn allocate(&self) -> Pointer;
+    fn allocate(&mut self) -> Pointer; //
 
     // function belong to a namespace
     fn function(&self, namespace: Namespace, name: &String) -> Option<Callable>;
@@ -42,16 +46,203 @@ trait KnowledgeBase {
     fn types_with_attribute(&self, name: &String) -> &HashSet<Type>;
 }
 
+struct Scope {
+    // depending on the execution path, a variable name can refer to multiple things
+    identifiers: HashMap<String, Vec<HashSet<Pointer>>>,
+}
+
+impl Scope {
+    fn new() -> Scope {
+        Scope {
+            identifiers: HashMap::new(),
+        }
+    }
+
+    fn assign_to_identifier(&mut self, name: &String, possibilities: HashSet<Pointer>) {
+        match self.identifiers.entry(name.clone()) {
+            Vacant(entry) => {
+                let list = entry.insert(Vec::new());
+                list.push(possibilities);
+            },
+            Occupied(mut entry) => {
+                let list = entry.get_mut();
+                list.push(possibilities);
+            }
+        };
+    }
+
+    fn resolve_identifier(&self, name: &String) -> Option<&HashSet<Pointer>> {
+        match self.identifiers.get(name) {
+            Some(list) => return list.last(),
+            None => None,
+        }
+    }
+
+    // takes ownership of self, dies in the process
+    fn merge_into(self, target: &mut Scope) {
+        for (name, mut list) in self.identifiers {
+            match target.identifiers.entry(name) {
+                Vacant(entry) => {
+                    let own_list = entry.insert(Vec::new());
+                    own_list.append(&mut list);
+                },
+                Occupied(mut entry) => {
+                    let result = Vec::new();
+
+                    // let own_list go out of scope before inserting
+                    {
+                        // take ownership of the current content, effectively removing it
+                        let own_list = entry.get();
+
+                        // improved zip, more efficient as well
+                        let longest_list;
+                        let shortest_list;
+
+                        if list.len() > own_list.len() {
+                            longest_list = &list;
+                            shortest_list = own_list;
+                        } else {
+                            longest_list = own_list;
+                            shortest_list = &list;
+                        }
+
+                        let ref cut = longest_list[..shortest_list.len()];
+
+                        let mut result = Vec::new();
+
+                        // merge whatever they have in common
+                        for i in 0..cut.len() {
+                            let ref x = longest_list[i];
+                            let ref y = shortest_list[i];
+
+                            let merged: HashSet<Pointer> = x.union(&y).cloned().collect();
+                            result.push(merged);
+                        }
+
+                        // add the rest
+                        for i in cut.len()..longest_list.len() {
+                            let x: HashSet<Pointer> = longest_list[i].clone();
+                            result.push(x);
+                        }
+                    }
+
+                    // put the result back in the hashmap
+                    entry.insert(result);
+                }
+            }
+        }
+    }
+}
+
+struct Pls<'a> {
+    count: i16,
+
+    parent: Option<&'a Pls<'a>>,
+
+    objects: HashMap<Pointer, Object>,
+    function_definitions: HashMap<Pointer, FunctionDefinition>,
+    builtin_functions: HashMap<Pointer, BuiltinFunction>,
+
+    private_scope: Scope,
+    public_scope: Scope,
+
+    types: HashSet<Type>,
+    iterable_types: HashSet<Type>,
+    indexable_types: HashSet<Type>,
+    types_with_attribute: HashMap<String, Type>,
+    methods: HashMap<Type, HashMap<String, Callable>>,
+}
+
+impl<'a> Pls<'a> {
+    /// add new things to the knowledge base
+    fn new_object(&mut self) -> Pointer {
+        let index = self.count;
+        self.count += 1;
+
+        let object = Object::new(index);
+        self.objects.insert(index, object);
+
+        return index;
+    }
+
+    fn define_custom_function(&mut self, defintion: FunctionDefinition) {
+        let index = self.count;
+        self.count += 1;
+
+        self.function_definitions.insert(index, defintion);
+    }
+
+    fn define_builtin_function(&mut self, definition: BuiltinFunction) {
+        let index = self.count;
+        self.count += 1;
+
+        self.builtin_functions.insert(index, definition);
+    }
+
+    /// change things in the knowledge base
+    //todo remove these
+    fn link_objects(&mut self, parent_address: &Pointer, child_address: &Pointer) {
+        match self.objects.get_mut(child_address) {
+            Some(child) => {
+                child.set_parent(parent_address.clone());
+            },
+            None => panic!("Invalid Pointer Value")
+        }
+    }
+
+    fn limit_types(&mut self, address: &Pointer, limits: &HashSet<Type>) {
+        match self.objects.get_mut(address) {
+            Some(object) => {
+                object.limit_types(limits);
+            },
+            None => panic!("Invalid Pointer Value")
+        }
+    }
+
+    /// get things from the knowledge base
+    fn dereference_pointer(&self, address: &Pointer) -> &Object {
+        match self.objects.get(address) {
+            Some(def) => {
+                return def;
+            },
+            None => panic!("Invalid Pointer Value")
+        }
+    }
+
+    fn get_function_definition(&self, address: &Pointer) -> &FunctionDefinition {
+        match self.function_definitions.get(address) {
+            Some(def) => {
+                return def;
+            },
+            None => panic!("Invalid Pointer Value")
+        }
+    }
+
+    fn get_builtin_function(&self, address: &Pointer) -> &BuiltinFunction {
+        match self.builtin_functions.get(address) {
+            Some(def) => {
+                return def;
+            },
+            None => panic!("Invalid Pointer Value")
+        }
+    }
+
+}
+
 //todo implement for each builtin function
-trait BuiltinFunction {
-    fn call(&self, kb: &mut KnowledgeBase, args: [&Object]);
-}
-
-trait FunctionDefinition {
+struct BuiltinFunction {
 
 }
 
-trait Namespace {
+impl BuiltinFunction {
+    //fn call(&self, kb: &mut KnowledgeBase, args: [&Object]);
+}
+
+struct FunctionDefinition {
+
+}
+
+struct Namespace {
 
 }
 
@@ -92,13 +283,11 @@ impl Object {
         }
     }
 
-    fn limit_types(&mut self, kb: &KnowledgeBase, limits: &HashSet<Type>) {
+    fn limit_types(&mut self, limits: &HashSet<Type>) {
         // the more information we have about an object, the further we can limit the possible types
         match self.type_property {
             None => {
-                let all = kb.all_types();
-                let possible = all.intersection(limits).cloned().collect();
-                let property = TypedObject::new(possible);
+                let property = TypedObject::new(limits.clone());
                 self.type_property = Some(property);
             },
             Some(ref mut property) => {
@@ -107,12 +296,12 @@ impl Object {
         }
     }
 
-    fn iterate(&mut self, kb: &KnowledgeBase) -> Pointer {
+    fn iterate(&mut self, kb: &mut KnowledgeBase) -> Pointer {
         // limit the possible types to iterable types
         // return a reference to the object representing its kind of elements
         match self.iterable_property {
             None => {
-                self.limit_types(kb, kb.iterable_types());
+                self.limit_types(kb.iterable_types());
 
                 let address = kb.allocate();
                 let property = IterableObject::new(address);
@@ -125,12 +314,12 @@ impl Object {
         }
     }
 
-    fn index(&mut self, kb: &KnowledgeBase) -> Pointer {
+    fn index(&mut self, kb: &mut KnowledgeBase) -> Pointer {
         // limit the possible types to indexable types
         // return a reference to the object representing its kind of elements
         match self.indexable_property {
             None => {
-                self.limit_types(kb, kb.indexable_types());
+                self.limit_types(kb.indexable_types());
 
                 let address = kb.allocate();
                 let property = IndexableObject::new(address);
@@ -161,8 +350,9 @@ impl Object {
             },
         };
 
-        self.limit_types(kb, kb.types_with_attribute(name));
+        self.limit_types(kb.types_with_attribute(name));
 
+        //todo remove need for self.address, add reference to parent in every property?
         return self.composite_property.as_mut().unwrap().add_attribute(kb, name, self.address.clone());
     }
 
@@ -246,6 +436,7 @@ impl TypedObject {
 }
 
 struct CompositeObject {
+    //todo split in private/public/(protected)
     attributes: HashMap<String, Pointer>,
 }
 
